3415c1295ebd6f227f0f9d1fa966d75454eb01bf
Atul Pandey <85121357+Atulpandey79@users.noreply.github.com>
2021-09-15 10:37:13 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const selectElement = (s) => document.querySelector(s);
-
-selectElement('.open').addEventListener('click',() => {
-   selectElement('.nav-List').classList.add('active');
-});
-
-selectElement('.close').addEventListener('click',() => {
-   selectElement('.nav-List').classList.remove('active');
-})
